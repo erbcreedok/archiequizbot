cc8a749d8ed52c9732041a0d2ab12d492bd81747
Ербол Сыздық <ysyzdyk@alfabank.kz>
2020-06-09 00:32:51 +0600

    switch dates in polls and test_polls

--- index.js
+++ index.js
@@ -101,5 +101,5 @@ app.get('/', async (req, res) => {
 app.listen(process.env.PORT || 3000)
 
 setInterval(() => {
-  console.log('healt monitor is alive', moment().format('HH:mm DD.MM.YYYY'))
+  console.log('health monitor is alive', moment().format('HH:mm DD.MM.YYYY'))
 }, 1000 * 60 * 5)
